1dbdfb35515fd21388ea38856b897d43ad4a148c
Ahmed Salem <ahmed.salah98as@gmail.com>
2021-09-27 15:47:53 +0200

    First Review

--- src/client/index.js
+++ src/client/index.js
@@ -7,15 +7,7 @@ import './styles/header.scss'
 import './styles/form.scss'
 import './styles/footer.scss'
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js').then(registration => {
-        console.log('SW registered: ', registration);
-      }).catch(registrationError => {
-        console.log('SW registration failed: ', registrationError);
-      });
-    });
-}
+
 
 export {
     checkForURL,
